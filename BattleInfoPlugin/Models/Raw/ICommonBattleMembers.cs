7e60e775dd83c9bd4694e315753947c3120e190c
veigr <veigr@cat-ears.net>
2015-09-07 19:53:18 +0900

    うっかりeKyoukaを足してしまっていた問題を修正(演習にのみ影響のはず)

--- BattleInfoPlugin/Models/Raw/ICommonBattleMembers.cs
+++ BattleInfoPlugin/Models/Raw/ICommonBattleMembers.cs
@@ -25,10 +25,10 @@ namespace BattleInfoPlugin.Models.Raw
                 .Select((x, i) => new MastersShipData(master.Ships[x])
                 {
                     Level = data.api_ship_lv[i + 1],
-                    Firepower = data.api_eParam[i][0] + data.api_eKyouka[i][0],
-                    Torpedo = data.api_eParam[i][1] + data.api_eKyouka[i][1],
-                    AA = data.api_eParam[i][2] + data.api_eKyouka[i][2],
-                    Armer = data.api_eParam[i][3] + data.api_eKyouka[i][3],
+                    Firepower = data.api_eParam[i][0],
+                    Torpedo = data.api_eParam[i][1],
+                    AA = data.api_eParam[i][2],
+                    Armer = data.api_eParam[i][3],
                     Slots = data.api_eSlot[i]
                         .Where(s => 0 < s)
                         .Select(s => master.SlotItems[s])
